14ad0a8c84ada98abf787b1dcd139fa18f60217b
hyomine <gksgyals12322@gmail.com>
2023-06-05 20:51:25 +0900

    40초가 지나면 입력을 할 수 없음

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import './index.css';
 import AutosizeTextarea from 'react-autosize-textarea';
@@ -9,16 +8,29 @@ function App() {
   const [text, setText] = useState('');
   const [input, setInput] = useState('');
   const [textClass, setTextClass] = useState('');
-  const [startTime, setStartTime] = useState(null);
-  const [endTime, setEndTime] = useState(null);
   const [typingSpeed, setTypingSpeed] = useState(0);
+  const [timer, setTimer] = useState(40);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
 
   const handleLanguageClick = (language) => {
     setSelectedLanguage(language);
-    setText(Text(language));
+    generateNewText(language);
+    setInput('');
+    setIsTimerRunning(false);
+    setTypingSpeed(0);
+  };
+
+  const generateNewText = (language) => {
+    const newText = Text(language);
+    setText(newText);
   };
 
   const handleChange = (e) => {
+    if (timer <= 0) {
+      e.preventDefault();
+      return; // 타이머가 0일 때는 입력을 처리하지 않음
+    }
+
     const inputValue = e.target.value;
     setInput(inputValue);
 
@@ -44,41 +56,56 @@ function App() {
 
     setInput(updatedInput);
     setTextClass(isCorrect ? 'correct' : 'incorrect');
+
+    if (isCorrect && formattedInput === formattedText) {
+      generateNewText(selectedLanguage);
+      setInput('');
+      setIsTimerRunning(false);
+      setTypingSpeed(0);
+      setTimer(40); // 타이머 초기화
+    }
   };
 
+  useEffect(() => {
+    if (isTimerRunning && timer > 0) {
+      const timerId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(timerId);
+      };
+    } else if (timer === 0) {
+      
+    }
+  }, [isTimerRunning, timer]);
 
   useEffect(() => {
-    if (input.length === 0) {
-      setStartTime(null);
-      setEndTime(null);
-      setTypingSpeed(0);
-    }
-
-    if (input.length === 1 && !startTime) {
-      setStartTime(Date.now());
-    }
-
-    if (input.length === text.length && startTime) {
-      setEndTime(Date.now());
-    }
-  }, [input, text, startTime]);
-
-  useEffect(() => {
-    if (startTime && endTime) {
-      const elapsedTime = (endTime - startTime) / 1000;
-      const cpm = text.length / elapsedTime;
-      const wpm = cpm * (60 / 5);
+    if (!isTimerRunning && input.length > 0) {
+      const elapsedTime = 40 - timer;
+      const cpm = text.length / (elapsedTime / 60);
+      const wpm = cpm / 5;
       setTypingSpeed(wpm.toFixed(0));
     }
-  }, [text, startTime, endTime]);
+  }, [text, timer, isTimerRunning]);
 
   const formatTypingSpeed = (typingSpeed) => {
     if (typingSpeed > 0) {
-      const formattedSpeed = typingSpeed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      const formattedSpeed = typingSpeed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
       return formattedSpeed;
     }
     return typingSpeed;
   };
+
+  const handleInputFocus = () => {
+    setIsTimerRunning(true);
+  };
+
+  useEffect(() => {
+    if (!isTimerRunning) {
+      setTimer(40);
+    }
+  }, [isTimerRunning]);
 
   return (
     <div className="App">
@@ -120,12 +147,17 @@ function App() {
               rows={10}
               value={input}
               onChange={handleChange}
+              onFocus={handleInputFocus}
               className="typing-space"
-              placeholder="Start typing here..."
+              placeholder={timer > 0 ? "Start typing here..." : "Time's up!"}
+              disabled={timer <= 0}
             />
           </div>
           <div className="typing-speed">
             Your typing speed: <span className="speed">{formatTypingSpeed(typingSpeed)}</span> WPM
+          </div>
+          <div className="timer">
+            Time remaining: {timer}s
           </div>
         </>
       )}
@@ -134,4 +166,3 @@ function App() {
 }
 
 export default App;
-
